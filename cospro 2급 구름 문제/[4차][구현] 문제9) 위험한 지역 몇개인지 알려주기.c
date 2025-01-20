3bb36c6044dc4bd9090b6a4a141ced28b957c693
ijigu <75564572+zidudu@users.noreply.github.com>
2025-01-20 23:47:37 +0900

    Update [4차][구현] 문제9) 위험한 지역 몇개인지 알려주기.c

--- cospro 2급 구름 문제/[4차][구현] 문제9) 위험한 지역 몇개인지 알려주기.c
+++ cospro 2급 구름 문제/[4차][구현] 문제9) 위험한 지역 몇개인지 알려주기.c
@@ -22,3 +22,12 @@ int solution(int height[][4], int height_len) {
 		}	
     return count;
 	}
+
+
+int main() {
+    int height[4][4] = {{3, 6, 2, 8}, {7, 3, 4, 2}, {8, 6, 7, 3}, {5, 3, 2, 9}};
+    int height_len = 4;
+    int ret = solution(height, height_len = 4);
+
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+}
