00a3cdadc9172b69e568b40f6c43c60f9b46c723
ijigu <75564572+zidudu@users.noreply.github.com>
2025-04-04 14:50:11 +0900

    Update 1410 : 올바른 괄호 1 (괄호 개수 세기).c

--- CodeUp문제풀이/1401~1500 문제/1410 : 올바른 괄호 1 (괄호 개수 세기).c
+++ CodeUp문제풀이/1401~1500 문제/1410 : 올바른 괄호 1 (괄호 개수 세기).c
@@ -11,12 +11,12 @@ void main() {
 	//숫자 10개 입력
 	/*for (i = 0;i < 100;i++)*/
 
-	// sizeof(a)로 문자 값 100자만 받겠다는 의미
-	//  scanf로 쓸땐 scanf("%100s"a); 로 써야함
-	// scanf("%s"a); 로 쓰면 100자 이상을 받게 되서 스택 오버플로우가 발생
+	// sizeof(a)로 문자 값 100000자만 받겠다는 의미
+	//  scanf로 쓸땐 scanf("%100000s"a); 로 써야함
+	// scanf("%s"a); 로 쓰면 100000자 이상을 받게 되서 스택 오버플로우가 발생
 	fgets(a, sizeof(a), stdin);
 
-	//가로들을 세고 출력
+	//가로들을 셈.
 	for (int i = 0;a[i] != '\0';i++) {
 		if (a[i] == '(') a_count++;
 		else if (a[i] == ')') b_count++;
