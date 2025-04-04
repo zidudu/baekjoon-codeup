f4317b6e9d11be2bb37486d03ad333ea903a901d
ijigu <75564572+zidudu@users.noreply.github.com>
2025-04-04 14:49:10 +0900

    Add files via upload

---
+++ CodeUp문제풀이/1401~1500 문제/FileName.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+void main() {
+	char a[100001];
+	int k;
+	int i = 0;
+	int a_count = 0;
+	int b_count = 0;
+	//숫자 10개 입력
+	/*for (i = 0;i < 100;i++)*/
+
+	// sizeof(a)로 문자 값 100자만 받겠다는 의미
+	//  scanf로 쓸땐 scanf("%100s"a); 로 써야함
+	// scanf("%s"a); 로 쓰면 100자 이상을 받게 되서 스택 오버플로우가 발생
+	fgets(a, sizeof(a), stdin);
+
+	//가로들을 세고 출력
+	for (int i = 0;a[i] != '\0';i++) {
+		if (a[i] == '(') a_count++;
+		else if (a[i] == ')') b_count++;
+	}
+	// 개수 출력
+	printf("%d %d", a_count, b_count);
+
+
+
+
+
+
+
+
+}
